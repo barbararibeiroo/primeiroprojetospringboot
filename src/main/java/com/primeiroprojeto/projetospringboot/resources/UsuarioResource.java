bd589852779a166850201f2769192122b8ff999a
Bárbara Ribeiro <barbararibeiro18@gmail.com>
2020-10-01 00:23:48 -0300

    Usuário update

--- src/main/java/com/primeiroprojeto/projetospringboot/resources/UsuarioResource.java
+++ src/main/java/com/primeiroprojeto/projetospringboot/resources/UsuarioResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,4 +53,9 @@ public class UsuarioResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Usuario obj){
+		obj = servico.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
